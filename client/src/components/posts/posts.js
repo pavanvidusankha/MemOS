3f21c3fffb0b2990bd15e4d700c7c3816a5cf117
pavanvidusankha <pavansamaranayake@gmail.com>
2021-05-11 19:58:30 +0530

    added basic routers and components

---
+++ client/src/components/posts/posts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "./post/post";
+
+const Posts = () => {
+  return (
+    <React.Fragment>
+      <h1>Posts</h1>
+      <Post />
+      <Post />
+    </React.Fragment>
+  );
+};
+
+export default Posts;
