c129a92a3af0838cf5fabdc3b0cdbbf23b9010c9
pavanvidusankha <pavansamaranayake@gmail.com>
2021-05-11 15:18:11 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+//import App from '../client/src/app';
+
+const app= express();
+
+app.use(express.urlencoded({limit:"30mb",extended:true}));
+app.use(express.json({limit:"30mb",extended:true}));
+app.use(cors());
+
+const CONNECTION_URL='mongodb+srv://pavan:H9goeWOaOkc9k8Ym@cluster0.uunk4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT=process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>app.listen(PORT,()=>console.log(`Server is running on Port : ${PORT}`))).catch
+((error)=>console.log(error.message))
+
+mongoose.set('useFindAndModify',false);
