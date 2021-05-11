61bd87e3e8e5f6dc3c482d66ae491a232ce87f19
pavanvidusankha <pavansamaranayake@gmail.com>
2021-05-11 15:41:46 +0530

    added routers folder

--- server/index.js
+++ server/index.js
@@ -2,13 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-//import App from '../client/src/app';
+
+import postRoutes from './routes/posts.js';
 
 const app= express();
+
 
 app.use(express.urlencoded({limit:"30mb",extended:true}));
 app.use(express.json({limit:"30mb",extended:true}));
 app.use(cors());
+
+app.use('/posts',postRoutes);
 
 const CONNECTION_URL='mongodb+srv://pavan:H9goeWOaOkc9k8Ym@cluster0.uunk4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT=process.env.PORT || 5000;
